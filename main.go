c31528dc7d4eacf050bb81a253f7e36d5fe8eb83
alireza <alirezac257@gmail.com>
2023-11-18 02:21:07 +0330

    impl dynamic connection map

--- main.go
+++ main.go
@@ -5,11 +5,57 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"log"
 	"main/db"
 	"main/server"
 	"net/http"
 	"time"
 )
+
+func MakeNewServer() Server {
+	return Server{
+		connections: make(map[*websocket.Conn]bool),
+	}
+}
+
+type Server struct {
+	connections map[*websocket.Conn]bool
+}
+
+func (s *Server) NewConnectionHandler(c echo.Context) error {
+	WSConn, err := server.Upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		c.Logger().Error(err)
+	}
+	s.connections[WSConn] = true
+	fmt.Printf("loggin all connections ==> %+v", s.connections)
+	go s.ReadLoop(WSConn)
+
+	return nil
+}
+
+func (s *Server) ReadLoop(c *websocket.Conn) {
+	for {
+		_, msg, err := c.ReadMessage()
+		if err != nil {
+			break
+		}
+		fmt.Printf("new msg => %s", msg)
+		go s.broadCast(msg)
+	}
+}
+
+func (s *Server) broadCast(msg []byte) {
+	for con := range s.connections {
+		if s.connections[con] {
+			err := con.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				fmt.Printf("error writing msg to conn %v", con)
+				continue
+			}
+		}
+	}
+}
 
 func init() {
 	db.InitializeDatabase()
@@ -18,31 +64,12 @@ func init() {
 }
 
 func main() {
+	baseServer := MakeNewServer()
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.GET("/socket/", SocketHandler)
-	e.GET("/socket/ping/", Pinger)
+	e.GET("/socket/", baseServer.NewConnectionHandler)
 	e.Logger.Fatal(e.Start(":8000"))
-}
-
-func Pinger(c echo.Context) error {
-	WSConn, err := server.Upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		c.Logger().Error(err)
-	}
-	defer func(WSConn *websocket.Conn) {
-		err := WSConn.Close()
-		if err != nil {
-
-		}
-	}(WSConn)
-
-	err = WSConn.WriteControl(websocket.PongMessage, []byte("pong"), time.Now().Add(time.Hour))
-	if err != nil {
-		c.Logger().Error(err)
-	}
-	return nil
 }
 
 func SocketHandler(c echo.Context) error {
@@ -58,12 +85,22 @@ func SocketHandler(c echo.Context) error {
 	}(WSConn)
 	i := 0
 	for {
-		err = WSConn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("-`----1---->%v<--------", i)))
+		fmt.Println("azaval")
+		writer, err := WSConn.NextWriter(websocket.TextMessage)
 		if err != nil {
-			c.Logger().Error(err)
+			continue
 		}
+		_, err = writer.Write([]byte(fmt.Sprintf("gooooz --> %v", i)))
+		if err != nil {
+			continue
+		}
+		time.Sleep(time.Second * 5)
+		msgType, _, err := WSConn.NextReader()
+		if err != nil {
+			log.Printf("error reading msg => %v", err)
+			continue
+		}
+		fmt.Printf("\n-----received new msg -----\nmsgType ==> %+v", msgType)
 		i++
-		time.Sleep(time.Second)
-
 	}
 }
